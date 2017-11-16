689503e6b6a94e95ae8d103469e6c1532f997aac
Mike <misha-radchenko@yandex.ru>
2017-11-16 10:59:10 +0300

    API Urls controller + routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth'
+    get 'users/:user_id/urls', to: 'urls#index'
+    get 'users/:user_id/urls/:id', to: 'urls#show'
+    post 'users/:user_id/urls', to: 'urls#create'
+    delete 'users/:user_id/urls/:id', to: 'urls#destroy'
   end
 
 end
