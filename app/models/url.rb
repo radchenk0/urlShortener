849486a97d36546c580aa42053b4bef69a145b82
Mike <misha-radchenko@yandex.ru>
2017-11-16 11:06:19 +0300

    Validations for Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,7 @@
 class Url < ApplicationRecord
     belongs_to :user
+    validates :original, :user_id, :presence => true
+    validates :original, :successful_response => true, :uniqueness => true
+    validates :slug, :uniqueness => true
 
 end
