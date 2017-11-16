f383eba5b6158c9a7a81dcff6c220e194d133d32
Mike <misha-radchenko@yandex.ru>
2017-11-16 10:48:41 +0300

    Style sheets, meta tags, favicon and include angularJs in rails app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import uiRouter from '@uirouter/angularjs';
 import ngTokenAuth from 'ng-token-auth';
 import ipCookies from 'angular-cookie';
 
+import '../src/application.css';
+
 const APP_MODULE = angular.module('url-shortener', [
   uiRouter,
   ngTokenAuth,
