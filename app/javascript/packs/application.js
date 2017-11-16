5f7dfc0d9c56d2483a5ec02569dce605878b6e9f
Mike <misha-radchenko@yandex.ru>
2017-11-16 11:19:33 +0300

    Main Module config + enabling html5mode for angular routes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 import ngTokenAuth from 'ng-token-auth';
 import ipCookies from 'angular-cookie';
+import { MAIN_MODULE } from './main/main.module';
 
 import '../src/application.css';
 
@@ -9,4 +10,5 @@ const APP_MODULE = angular.module('url-shortener', [
   uiRouter,
   ngTokenAuth,
   ipCookies,
+  MAIN_MODULE.name
 ]);
---
+++ app/javascript/packs/main/main.module.js
@@ -0,0 +1,24 @@
+// modules import
+require('ngstorage');
+import collapse from 'angular-ui-bootstrap/src/collapse';
+import popover from 'angular-ui-bootstrap/src/popover';
+import alert from 'angular-ui-bootstrap/src/alert';
+import modal from 'angular-ui-bootstrap/src/modal';
+import ngMessages from 'angular-messages';
+import ngClipboard from 'ngclipboard';
+
+export const MAIN_MODULE = angular.module('main', [
+  'ngStorage',
+  collapse,
+  popover,
+  alert,
+  modal,
+  ngMessages,
+  ngClipboard
+]);
+
+MAIN_MODULE.config(($uiRouterProvider, $locationProvider) => {
+  $locationProvider.html5Mode(true);
+  $uiRouterProvider.trace.enable(1);
+});
+
